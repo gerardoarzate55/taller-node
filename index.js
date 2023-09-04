fe7266a4103deb933ee91e53c8b6f69012862ef3
Gerardo Arzate <gerardoarzate55@gmail.com>
2023-09-04 14:44:29 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const puerto = 12345;
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.listen(puerto, () => console.log(`Servidor iniciado en el puerto ${puerto}`));
