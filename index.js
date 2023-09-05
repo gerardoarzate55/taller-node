ded053d1c5c559edc8219da036f03f1d197c698b
Gerardo Arzate <gerardoarzate55@gmail.com>
2023-09-04 22:45:58 -0600

    Updated index.js (added status code)

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const { pokemon } = require('./pokedex.json');
 const puerto = 12345;
 
 app.get('/', (req, res) => {
+    res.status(200);
     res.send('Bienvenido al Pokédex');
 });
 
 app.get('/pokemon', (req, res) => {
+    res.status(200);
     res.send(pokemon);
 });
 
 app.get('/pokemon/:pokeId', (req, res) => {
     const { pokeId } = req.params;
     const pokeData = pokemon[pokeId - 1];
+
+    res.status(pokeData != undefined ? 200 : 404);
 
     res.send(pokemon[pokeId - 1]);
 })
