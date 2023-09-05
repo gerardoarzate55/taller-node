4e88fe730e9a6691f5b6eeaad932d9d97d09f16a
Gerardo Arzate <gerardoarzate55@gmail.com>
2023-09-04 22:43:03 -0600

    Updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
+
 const puerto = 12345;
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo');
+    res.send('Bienvenido al Pokédex');
 });
 
-app.get('/:name', (req, res) => {
-    res.send(`Hola ${req.params.name}!`);
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:pokeId', (req, res) => {
+    const { pokeId } = req.params;
+    const pokeData = pokemon[pokeId - 1];
+
+    res.send(pokemon[pokeId - 1]);
 })
 
 app.listen(process.env.PORT || puerto, () => console.log(`Servidor iniciado en el puerto ${puerto}`));
