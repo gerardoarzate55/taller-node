4386a0b86073e9f1e934d346818ec177db9330c9
Gerardo Arzate <gerardoarzate55@gmail.com>
2023-09-04 20:22:32 -0600

    Added listener for /:name

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hola mundo');
 });
 
+app.get('/:name', (req, res) => {
+    res.send(`Hola ${req.params.name}!`);
+})
+
 app.listen(process.env.PORT || puerto, () => console.log(`Servidor iniciado en el puerto ${puerto}`));
