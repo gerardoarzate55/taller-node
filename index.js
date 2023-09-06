db6c80a725c0bc1d310c0b7ef0b63d5192f08d91
Gerardo Arzate <gerardoarzate55@gmail.com>
2023-09-05 21:27:33 -0600

    Updated index.js (new routes added)

--- index.js
+++ index.js
@@ -9,18 +9,37 @@ app.get('/', (req, res) => {
     res.send('Bienvenido al Pokédex');
 });
 
-app.get('/pokemon', (req, res) => {
+app.get('/pokemon/all', (req, res) => {
     res.status(200);
     res.send(pokemon);
 });
 
-app.get('/pokemon/:pokeId', (req, res) => {
+app.get('/pokemon/:pokeId([0-9]{1,3})', (req, res) => {
     const { pokeId } = req.params;
     const pokeData = pokemon[pokeId - 1];
 
-    res.status(pokeData != undefined ? 200 : 404);
-
-    res.send(pokemon[pokeId - 1]);
+    if (pokeData == undefined) {
+        res.status(404);
+        res.send('Pokémon no encontrado');
+        return;
+    }
+    
+    res.status(200);
+    res.send(pokeData);
 })
+
+app.get('/pokemon/:pokeName', (req, res) => {
+    const pokeName = req.params.pokeName.toLowerCase();
+    const pokeData = pokemon.find(pkm => pokeName == pkm.name.toLowerCase());
+
+    if (pokeData == undefined) {
+        res.status(404);
+        res.send('Pokémon no encontrado');
+        return;
+    }
+
+    res.status(200);
+    res.send(pokeData);
+});
 
 app.listen(process.env.PORT || puerto, () => console.log(`Servidor iniciado en el puerto ${puerto}`));
